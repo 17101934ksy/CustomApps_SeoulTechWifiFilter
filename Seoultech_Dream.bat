3fb5f4f60c520d41076f68b0c8cb40ae0973c442
Koseyun <88478829+17101934ksy@users.noreply.github.com>
2022-06-01 14:51:47 +0900

    Delete Seoultech_Dream.bat

--- Seoultech_Dream.bat
+++
@@ -1,4 +0,0 @@
-netsh wlan add filter permission=denyall networktype=infrastructure 
-netsh wlan add filter permission=allow ssid="Seoultech_Dream" networktype=infrastructure 
-netsh wlan connect name="Seoultech_Dream" ssid="Seoultech_Dream" 
-netsh wlan set profileparameter name="Seoultech_Dream" connectionmode=auto
